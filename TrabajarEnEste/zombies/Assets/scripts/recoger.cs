5c380461750bfd982a505bc4e2be855a373e841f
josemaria mira barrientos <josemariamira@hotmail.com>
2015-05-08 13:21:51 -0500

    recogiendo armas , mal implementado

--- TrabajarEnEste/zombies/Assets/scripts/recoger.cs
+++ TrabajarEnEste/zombies/Assets/scripts/recoger.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class recoger : MonoBehaviour {
 
 	public Transform personaje;
+	float rotationSpeed = 3f; 
 	bool agarrado = false;
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (agarrado == true){
-			transform.position = new Vector3 (personaje.position.x - 1.28f, personaje.position.y + 0.83f, personaje.position.z + 0.47f);
-			transform.Rotate (personaje.rotation.x, personaje.rotation.y, personaje.rotation.z);
+			transform.position = new Vector3 (personaje.position.x /*- 1.28f*/, personaje.position.y/* + 0.83f*/, personaje.position.z /*+ 0.47f*/);
+			//transform.Rotate(0, personaje.rotation.y, 0);
 		}
 		if(Input.GetKey(KeyCode.T)){
 			agarrado = false;
